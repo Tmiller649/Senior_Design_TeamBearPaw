b15ad2185e1b8036e8d975eec634eef9a545e238
Terrance Miller <TerranceMillerh01@gmail.com>
2024-03-28 16:38:43 -0400

    Update MealRec.js

--- Website_Code/project_code/src/MealRec.js
+++ Website_Code/project_code/src/MealRec.js
@@ -7,7 +7,6 @@ function MealRec() {
           <div className="flip-card-inner">
             <div className="flip-card-front">
               <h2 id="MealName2">Meal Name 2</h2>
-              <a href="">Instructions</a>
             </div>
             <div className="flip-card-back">
               <h3>Nutritional Data</h3>
@@ -17,6 +16,27 @@ function MealRec() {
               <p>Sugars: <span className="Sugars">100</span></p>
               <p>Sodium: <span className="Sodium">100</span></p>
               <p>Protein: <span className="Protein">20</span></p>
+              <a href="">Instructions</a>
+              <input type="button" value="Pick Meal" id="PickMealOne"></input>
+            </div>
+          </div>
+        </div>
+  
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <h2 id="MealName2">Meal Name 2</h2>
+            </div>
+            <div className="flip-card-back">
+              <h3>Nutritional Data</h3>
+              <p>Calories: <span className="Calories">800</span></p>
+              <p>Calories-From-Fat: <span className="Calories-From-Fat">400</span></p>
+              <p>Carbohydrates: <span className="Carbs">200</span></p>
+              <p>Sugars: <span className="Sugars">100</span></p>
+              <p>Sodium: <span className="Sodium">100</span></p>
+              <p>Protein: <span className="Protein">20</span></p>
+              <a href="">Instructions</a>
+              <input type="button" value="Pick Meal" id="PickMealTwo"></input>
             </div>
           </div>
         </div>
@@ -35,24 +55,8 @@ function MealRec() {
               <p>Sugars: <span className="Sugars">100</span></p>
               <p>Sodium: <span className="Sodium">100</span></p>
               <p>Protein: <span className="Protein">20</span></p>
-            </div>
-          </div>
-        </div>
-  
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <h2 id="MealName2">Meal Name 2</h2>
               <a href="">Instructions</a>
-            </div>
-            <div className="flip-card-back">
-              <h3>Nutritional Data</h3>
-              <p>Calories: <span className="Calories">800</span></p>
-              <p>Calories-From-Fat: <span className="Calories-From-Fat">400</span></p>
-              <p>Carbohydrates: <span className="Carbs">200</span></p>
-              <p>Sugars: <span className="Sugars">100</span></p>
-              <p>Sodium: <span className="Sodium">100</span></p>
-              <p>Protein: <span className="Protein">20</span></p>
+              <input type="button" value="Pick Meal" id="PickMealThree"></input>
             </div>
           </div>
         </div>
