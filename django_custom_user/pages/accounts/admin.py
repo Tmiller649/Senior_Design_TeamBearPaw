d7cf0e6171fe376ad5d3fe818ffd8ec41024fa81
Trung Nguyen <trungnguyen212001@gmail.com>
2024-03-29 17:16:26 -0400

    add Django account server to project

---
+++ django_custom_user/pages/accounts/admin.py
@@ -0,0 +1,32 @@
+# accounts/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ("email", "weight", "height", "age", "gender", "physical_activity_level", "track_fat", "track_salt", "track_sugar", "weight_goal",)
+    list_filter = ("email", "weight", "height", "age", "gender", "physical_activity_level", "track_fat", "track_salt", "track_sugar", "weight_goal",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ django_custom_user/pages/accounts/models.py
@@ -0,0 +1,53 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .manager import CustomUserManager
+
+# iterable 
+SUB_GOAL_CHOICES =( 
+    ("FA", "Fat"), 
+    ("SA", "Salt"), 
+    ("SU", "Sugar"), 
+)
+PHYS_LVL_CHOICES =( 
+    ("S", "Sedentary"), 
+    ("LA", "Low Active"), 
+    ("A", "Active"),
+    ("VA", "Very Active") 
+)  
+WLP_CHOICES =( 
+    ("G", "Gain"), 
+    ("S", "Stay"), 
+    ("L", "Loss"), 
+)
+GENDER_CHOICES = (
+    ("M", "Male"),
+    ("F", "Female"),
+    ("N", "Non-Binary"),
+    ("P", "Prefer not to choose")
+) 
+
+class CustomUser(AbstractUser):
+    pass
+    # add additional fields in here
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    weight = models.FloatField(_("weight"), help_text = "lbs")
+    height = models.FloatField(_("height"), help_text = "in")
+    age = models.IntegerField(_("age"))
+    gender = models.CharField(max_length=1, choices = GENDER_CHOICES, default= "P")
+    physical_activity_level = models.CharField(max_length=100, choices=PHYS_LVL_CHOICES, default = "N/A")
+    track_fat = models.BooleanField(default = False, help_text = "Sub goal #1")
+    track_salt = models.BooleanField(default = False, help_text = "Sub goal #2")
+    track_sugar = models.BooleanField(default = False, help_text = "Sub goal #3")
+    weight_goal = models.CharField(max_length=4, choices=WLP_CHOICES, default = "L")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["weight", "height", "age", "gender", "physical_activity_level", "track_fat", "track_salt", "track_sugar", "weight_goal",]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
