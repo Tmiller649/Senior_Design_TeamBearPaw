a17de62f6c8c0ed9ecafde94839af6b3c4e543ed
Chau Tran <trhmc1811@gmail.com>
2024-04-03 23:34:07 -0400

    minor edits

--- DjangoBackend/DjangoBackend/settings.py
+++ DjangoBackend/DjangoBackend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'DietTracker', # needs editting
+    'DietTracker', # needs editting 'DietTracker.apps.DiettrackerConfig' + 'rest_framework'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
