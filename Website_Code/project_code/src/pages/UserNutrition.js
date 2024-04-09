f3edb710c722a5fbc848820073be8de616ba1fb0
Trung Nguyen <trungnguyen212001@gmail.com>
2024-04-09 00:46:26 -0400

    Change daily progress value to 0

--- Website_Code/project_code/src/pages/UserNutrition.js
+++ Website_Code/project_code/src/pages/UserNutrition.js
@@ -38,33 +38,33 @@ export default function UserNutrition() {
             <h2> Daily Progress Tracker</h2>
             <div className="ProgressBar">
             <label htmlFor="Calories">Calories</label>
-            <progress id="CaloriesProgress" value="1400" name="Calories" max={singleuser.get_nutrition.Calories[1]}></progress>
-            <p><span className="currCount">1400</span>/<span className="totCount">{singleuser.get_nutrition.Calories[1]}</span></p>
+            <progress id="CaloriesProgress" value="0" name="Calories" max={singleuser.get_nutrition.Calories[1]}></progress>
+            <p><span className="currCount">0</span>/<span className="totCount">{singleuser.get_nutrition.Calories[1]}</span></p>
             </div>
             <div className="ProgressBar">
             <label htmlFor="Calories-From-Fat">Calories-From-Fat</label>
-            <progress id="FatProgress" value="20" name="Calories-From-Fat" max={singleuser.get_nutrition.FatContent[1]}></progress>
-            <p><span className="currCount">20</span>/<span className="totCount">{singleuser.get_nutrition.FatContent[1]}</span>g</p>
+            <progress id="FatProgress" value="0" name="Calories-From-Fat" max={singleuser.get_nutrition.FatContent[1]}></progress>
+            <p><span className="currCount">0</span>/<span className="totCount">{singleuser.get_nutrition.FatContent[1]}</span>g</p>
             </div>
             <div className="ProgressBar">
             <label htmlFor="Carbs">Carbohydrates</label>
-            <progress id="CarbProgress" value="255" name="Carbs" max={singleuser.get_nutrition.CarbohydrateContent[1]}></progress>
-            <p><span className="currCount">255</span>/<span className="totCount">{singleuser.get_nutrition.CarbohydrateContent[1]}</span>g</p>
+            <progress id="CarbProgress" value="0" name="Carbs" max={singleuser.get_nutrition.CarbohydrateContent[1]}></progress>
+            <p><span className="currCount">0</span>/<span className="totCount">{singleuser.get_nutrition.CarbohydrateContent[1]}</span>g</p>
             </div>
             <div className="ProgressBar">
             <label htmlFor="Sugars">Sugars</label>
-            <progress id="SugarProgress" value="100" name="Sugars" max={singleuser.get_nutrition.SugarContent[1]}></progress>
-            <p><span className="currCount">100</span>/<span className="totCount">{singleuser.get_nutrition.SugarContent[1]}</span>g</p>
+            <progress id="SugarProgress" value="0" name="Sugars" max={singleuser.get_nutrition.SugarContent[1]}></progress>
+            <p><span className="currCount">0</span>/<span className="totCount">{singleuser.get_nutrition.SugarContent[1]}</span>g</p>
             </div>
             <div className="ProgressBar">
             <label htmlFor="Sodium">Sodium</label>
-            <progress id="SodiumProgress" value="1200" name="Sugars" max={singleuser.get_nutrition.SodiumContent[1]}></progress>
-            <p><span className="currCount">1200</span>/<span className="totCount">{singleuser.get_nutrition.SodiumContent[1]}</span>mg</p>
+            <progress id="SodiumProgress" value="0" name="Sugars" max={singleuser.get_nutrition.SodiumContent[1]}></progress>
+            <p><span className="currCount">0</span>/<span className="totCount">{singleuser.get_nutrition.SodiumContent[1]}</span>mg</p>
             </div>
             <div className="ProgressBar">
             <label htmlFor="Protein">Protein</label>
-            <progress id="ProteinProgress" value="100" name="Sugars" max={singleuser.get_nutrition.ProteinContent[1]}></progress>
-            <p><span className="currCount">100</span>/<span className="totCount">{singleuser.get_nutrition.ProteinContent[1]}</span>g</p>
+            <progress id="ProteinProgress" value="0" name="Sugars" max={singleuser.get_nutrition.ProteinContent[1]}></progress>
+            <p><span className="currCount">0</span>/<span className="totCount">{singleuser.get_nutrition.ProteinContent[1]}</span>g</p>
             </div>
             </div>
         ):null}
