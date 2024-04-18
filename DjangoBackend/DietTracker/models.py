ab70b48cef5586f76b7378da38a638cceac3ca8d
Chau Tran <trhmc1811@gmail.com>
2024-04-18 15:44:56 -0400

    delete uneeded folders

--- DjangoBackend/DietTracker/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# Health class, should take these info from a database
-class Health(models.Model):
-    age = models.IntegerField(null=False, default = 0)
